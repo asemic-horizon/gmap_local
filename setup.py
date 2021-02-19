dfa8d7ae33a77c0cafdede82df4235c3f4c06422
asemic horizon <42865953+asemic-horizon@users.noreply.github.com>
2021-02-19 10:03:38 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from setuptools import setup
+import os
+
+
+
+core_dependencies = []
+
+print(core_dependencies)
+setup(name="gmaps_local",
+      version='0.01',
+      description='APIs',
+      author='Diego Navarro',
+      author_email='the.electric.me@gmail.com',
+      license='MIT',
+      #packages=setuptools.find_packages(),
+      # install_requires=deps,
+      # dependency_links=core_dependencies,
+      zip_safe=False)
+
